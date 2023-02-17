daba02799de665c9092df5fcdde8dd3047164d3f
dadalee51 <dadalee51@gmail.com>
2023-02-17 11:46:14 +0800

    update robot gps location

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const MongoClient = require('mongodb').MongoClient;
 const app = express();
 const port = 3000;
 const url = 'mongodb://127.0.0.1:27017';
-const dbName = 'accounts-payable';
-const collectionName = 'invoices';
+const dbName = 'robotics_db';
+const collectionName = 'geolocation';
 app.use(express.static(path.join(__dirname,'public')));
 
 app.use(bodyParser.json());
@@ -18,8 +18,8 @@ MongoClient.connect('mongodb://127.0.0.1:27017', {
   };
   db.close();
  });
-// Create a new invoice
-app.post('/api/invoices', async (req, res) => {
+// Create a new coordinate
+app.post('/api/coord', async (req, res) => {
   try {
     const client = await MongoClient.connect(url,{
       useUnifiedTopology:true, useNewUrlParser:true
@@ -31,30 +31,30 @@ app.post('/api/invoices', async (req, res) => {
     res.send(result.ops[0]);
   } catch (err) {
     console.error(err);
-    res.status(500).send('Error creating invoice');
+    res.status(500).send('Error creating coord');
   }
 });
 
-// Get all invoices
-app.get('/api/invoices', async (req, res) => {
+// Get all coordinates
+app.get('/api/coord', async (req, res) => {
   try {
     const client = await MongoClient.connect(url,{
       useUnifiedTopology:true, useNewUrlParser:true
     });
     const db = client.db(dbName);
     const collection = db.collection(collectionName);
-    const invoices = await collection.find({}).toArray();
+    const coords = await collection.find({}).toArray();
     client.close();
-    res.send(invoices);
+    res.send(coords);
   } catch (err) {
     console.error(err);
-    res.status(500).send('Error getting invoices');
+    res.status(500).send('Error getting coords');
   }
 });
 
 //send root html
 app.get('/', (req,res) => {
-  res.sendFile(path.join(__dirname, 'public', 'invoice.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
