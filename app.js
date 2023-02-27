e0a0ed10fd95b25fb6cf30dbc8d8615338cebb54
dadalee51 <dadalee51@gmail.com>
2023-02-27 11:09:51 +0800

    update delete method

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const MongoClient = require('mongodb').MongoClient;
 const app = express();
 const port = 3000;
 const url = 'mongodb://127.0.0.1:27017';
-const dbName = 'robotics_db';
-const collectionName = 'geolocation';
+const dbName = 'student_credits';
+const collectionName = 'credits';
 app.use(express.static(path.join(__dirname,'public')));
 
 app.use(bodyParser.json());
@@ -18,8 +18,8 @@ MongoClient.connect('mongodb://127.0.0.1:27017', {
   };
   db.close();
  });
-// Create a new coordinate
-app.post('/api/coord', async (req, res) => {
+// Create 
+app.post('/api/credit', async (req, res) => {
   try {
     const client = await MongoClient.connect(url,{
       useUnifiedTopology:true, useNewUrlParser:true
@@ -31,24 +31,42 @@ app.post('/api/coord', async (req, res) => {
     res.send(result.ops[0]);
   } catch (err) {
     console.error(err);
-    res.status(500).send('Error creating coord');
+    res.status(500).send('Error creating credit');
   }
 });
 
-// Get all coordinates
-app.get('/api/coord', async (req, res) => {
+// Get all 
+app.get('/api/credit', async (req, res) => {
   try {
     const client = await MongoClient.connect(url,{
       useUnifiedTopology:true, useNewUrlParser:true
     });
     const db = client.db(dbName);
     const collection = db.collection(collectionName);
-    const coords = await collection.find({}).toArray();
+    const credits = await collection.find({}).toArray();
     client.close();
-    res.send(coords);
+    res.send(credits);
   } catch (err) {
     console.error(err);
-    res.status(500).send('Error getting coords');
+    res.status(500).send('Error getting credits');
+  }
+});
+
+// Delete specified entry 
+app.delete('/api/credit/:id', async (req, res) => {
+  try {
+    const client = await MongoClient.connect(url,{
+      useUnifiedTopology:true, useNewUrlParser:true
+    });
+    const db = client.db(dbName);
+    const collection = db.collection(collectionName);
+    const credits = await collection.findOneAndDelete({'student_number':req.params.id});
+    client.close();
+    console.log(credits);
+    res.send(`student ${credits.value.student_number} deleted.`);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error deleting credits');
   }
 });
 
